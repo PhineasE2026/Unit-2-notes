ea1833ff231962547704a6a51c895ca7482f82a5
finny <phineas.ehrlich2026@gbwl.org>
2024-09-19 12:56:11 +0000

    created Creature class.. plug walk

---
+++ Creature.java
@@ -0,0 +1,22 @@
+// Some Java CLASSES define OBJECTS
+// These classes are like blueprints or templates
+// You can create OBJECTS from a CLASS like this
+// OBJECTS are INSTANCES of a certain class
+public class Creature {
+    // INSTANCE VARIABLES
+    // Every Creature has certain attributes
+    String species;
+    double weight;
+    int numLegs;
+    boolean canPet;
+
+    // METHODS
+    // Every creature has certain behaviors
+    public void move() {
+
+    }
+    public void play() {
+
+    }
+
+}
