a94737b63ad279d042a4cd5a0b8a1034ab7857be
finny <phineas.ehrlich2026@gbwl.org>
2024-10-07 13:39:52 +0000

    lfksdfksfdkk;e;k

--- Main.java
+++ Main.java
@@ -96,6 +96,37 @@ public class Main {
       System.out.println(a.compareTo(z)); // -25
       System.out.println(z.compareTo(a)); // 25
 
+      // math class methods
+      // do not need to construct a math object to use
+
+      int absVal = Math.abs(-273);
+      System.out.println(absVal);
+      double absValDoub = Math.abs(-2.5);
+      System.out.println(absValDoub);
+      
+      // Math.pow(double, double) returns the first double raised to the power of the second double provided
+      double cubed = Math.pow(38, 3);
+      System.out.println(cubed);
+
+      // Math.sqrt(double)
+      double squareRoot = Math.sqrt(49);
+      System.out.println("square root of 49 = " + squareRoot); // abcdefghijklmnopqrstuvwxyz
+
+      //Math.random() returns a random double
+      System.out.println("random number: " + Math.random());
+
+      // modify the range of output from math.random
+      // Multiply output to increase end range
+      double rand = Math.random() * 100;
+      System.out.println(rand);
+      // add to output to increase starting point
+      double rand2 = Math.random() * 100 + 1;
+      System.out.println(rand2);
+
+      // Cast to an int if you want a random whole number
+      int randInt = (int) (Math.random() * 10 + 1);
+      System.out.println(randInt);
+
 
    }
 }
